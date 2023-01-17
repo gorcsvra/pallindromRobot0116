e40c3d94d32b2c1e38b5119484cfc0dd53872617
gorcsvra <66862598+gorcsvra@users.noreply.github.com>
2023-01-17 06:32:58 +0100

    indulés elötti commit

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,10 @@
-document.getElementById("input").addEventListener("focus", function () {
-  this.value = "";
-});
-
 //Function to check if the entered text is a palindrome
 function isPalindrome() {
   const input = document.getElementById("input").value;
   const cleanedString = input.replace(/\s+/g, "").toLowerCase();
   const result = cleanedString === cleanedString.split("").reverse().join("");
   const resu = document.getElementById("result");
+  const robot = document.querySelector(".robot");
   setTimeout(() => {
     resu.innerHTML = result;
     if (result) {
@@ -15,17 +12,20 @@ function isPalindrome() {
     } else {
       resu.style.color = "red";
     }
+    robot.style.display = "block";
     setTimeout(() => {
       resu.innerHTML = "";
+      robot.style.display = "none";
     }, 1000);
   }, 2000);
 }
 
 //Event listener to the button
-document.querySelector(".button").addEventListener("click", function () {
-  document.querySelector(".robot").style.display = "block";
-  isPalindrome();
-});
+document
+  .querySelector(".inputbutton button")
+  .addEventListener("click", function () {
+    isPalindrome();
+  });
 
 //Hiding the robot class
 document.querySelector(".robot").style.display = "none";
